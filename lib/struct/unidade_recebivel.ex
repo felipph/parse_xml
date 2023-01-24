0426257f00ef7123c37415af3175bacb5d0d0b98
Felipph Calado <luizfelipph@mail.com>
2023-01-24 20:50:40 -0300

    refactor e ajuste em alguns tipos

--- lib/struct/unidade_recebivel.ex
+++ lib/struct/unidade_recebivel.ex
@@ -1,11 +1,11 @@
 defmodule Struct.UnidadeRecebivel do
   @type t :: %__MODULE__{
+          cd_arranjo: String.t(),
           cnpf_cnpf_ufr: number(),
           dt_prevt_liq: Date.t(),
-          vlr_prevt_liq: String.t(),
-          dt_ref: Date.t(),
-          cd_arranjo: String.t()
+          vlr_prevt_liq: Decimal.t(),
+          vlr_total: Decimal.t()
         }
-
-  defstruct [:cnpf_cnpf_ufr, :cd_arranjo, :dt_prevt_liq, :vlr_prevt_liq, :dt_ref]
+  defstruct [:cnpf_cnpf_ufr, :cd_arranjo,
+  :dt_prevt_liq, :vlr_prevt_liq, :vlr_total]
 end
